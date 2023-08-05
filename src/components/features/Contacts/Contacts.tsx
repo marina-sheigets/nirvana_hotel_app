29d4bcee03425bab31b26175aae4182b5dfdf2fd
Marina Sheigets <sejmar1705@gmail.com>
2023-08-05 20:57:34 +0300

    feat: add contacts and map comps

--- src/components/features/Contacts/Contacts.tsx
+++ src/components/features/Contacts/Contacts.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { styled } from "styled-components";
 
 function Contacts() {
   return (
-    <div>Contacts</div>
-  )
+    <Wrapper>
+      <h1>Контакти</h1>
+      <p>
+        +38 (098) 04101 08 <br />
+        +38 (096) 463 84 51
+      </p>
+      <p>Nirvanarivne@ukr.net</p>
+      <p>​вулиця Княгині Ольги 8, Рівне, Рівненська область</p>
+    </Wrapper>
+  );
 }
 
-export default Contacts
+const Wrapper = styled("div")`
+  display: flex;
+  flex-direction: column;
+  background: #7a282c;
+  padding: 0 4rem 2rem;
+  gap: 1rem;
+  color: white;
+  p {
+    padding: 0;
+    margin: 0;
+    font-weight: 300;
+  }
+`;
+
+export default Contacts;
--- src/components/features/Map/Map.tsx
+++ src/components/features/Map/Map.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { styled } from "styled-components";
 
 function Map() {
   return (
-    <div>Map</div>
-  )
+    <Wrapper>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5813.263066461879!2d26.255333365801192!3d50.61167436525443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472f1344f3b3daa9%3A0x74d3032634649e9e!2sNirvana!5e0!3m2!1sru!2sua!4v1691257213869!5m2!1sru!2sua"
+        width="600"
+        height="450"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </Wrapper>
+  );
 }
 
-export default Map
+const Wrapper = styled("div")``;
+export default Map;
