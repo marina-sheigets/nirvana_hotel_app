5a74b66d0d592652962ddb9c92d4667b7afe9332
Marina Sheigets <sejmar1705@gmail.com>
2023-08-05 18:30:39 +0300

    feat: add banner component

--- src/components/features/Banner/Banner.tsx
+++ src/components/features/Banner/Banner.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import { styled } from 'styled-components'
+import img from '../../../assets/flowers-backgrd.jpg'
 function Banner() {
   return (
-    <div>Banner</div>
+    <Wrapper>
+      <BackgroundImgWrapper> 
+        <Header>Про готель</Header>
+        </BackgroundImgWrapper>
+    </Wrapper>
   )
 }
 
-export default Banner
+const Wrapper = styled('div')`
+`
+const BackgroundImgWrapper = styled('div')`
+display:flex;
+justify-content:center;
+align-items:center;
+  height:500px;
+  background: linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${img});
+  background-size: cover;
+`
+
+const Header = styled('h2')`
+  color:white;
+  font-size:4rem;
+`
+
+export default Banner;
