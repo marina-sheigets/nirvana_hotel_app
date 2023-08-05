f3a115ecd4d244a1c8cbef619c9badf6b4775bb5
Marina Sheigets <sejmar1705@gmail.com>
2023-08-05 20:08:04 +0300

    feat: add footer

--- src/components/features/Footer/Footer.tsx
+++ src/components/features/Footer/Footer.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import  { useEffect, useState } from "react";
+import { styled } from "styled-components";
 
 function Footer() {
+  const [currentYear, setCurrentYear] = useState<number>(0);
+
+  useEffect(() => {
+    const year = new Date().getFullYear();
+    setCurrentYear(year);
+  }, []);
   return (
-    <div>Footer</div>
-  )
+    <Wrapper>
+      <Text>{`Nirvana © ${currentYear} All Rights Reserved`}</Text>
+    </Wrapper>
+  );
 }
 
-export default Footer
+const Wrapper = styled("footer")`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: black;
+  height: 120px;
+`;
+const Text = styled("p")`
+  color: white;
+  font-size: 14px;
+  font-weight: 200;
+`;
+
+export default Footer;
