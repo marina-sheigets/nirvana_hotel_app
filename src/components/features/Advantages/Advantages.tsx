bc6475074befc34fa66b5108c59ac7a34ed439c1
Marina Sheigets <sejmar1705@gmail.com>
2023-08-05 19:51:04 +0300

    feat: add advantages component

--- src/components/features/Advantages/Advantages.tsx
+++ src/components/features/Advantages/Advantages.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import { styled } from "styled-components";
+import img from "../../../assets/flowers-backgrd.jpg";
+import { LuClock9, LuWifi, LuCigarette } from "react-icons/lu";
+import { GiShoppingCart } from "react-icons/gi";
+import { CiParking1 } from "react-icons/ci";
+import { BsClockHistory } from "react-icons/bs";
 
 function Advantages() {
+  const advantages = [
+    {
+      id: 1,
+      label: "Цілодобове поселення",
+      icon: <LuClock9 />,
+    },
+    {
+      id: 2,
+      label: "Wi-Fi",
+      icon: <LuWifi />,
+    },
+    {
+      id: 3,
+      label: "Номери для курців та некурців",
+      icon: <LuCigarette />,
+    },
+    {
+      id: 4,
+      label: "​Міні-магазин та кавовий апарат на території",
+      icon: <GiShoppingCart />,
+    },
+    {
+      id: 5,
+      label: "Безкоштовна парковка",
+      icon: <CiParking1 />,
+    },
+    {
+      id: 6,
+      label: "Погодинне поселення",
+      icon: <BsClockHistory />,
+    },
+  ];
   return (
-    <div>Advantages</div>
-  )
+    <Wrapper>
+      <AdvantagesList>
+        <h2>Переваги</h2>
+        {advantages.map((elem) => (
+          <Item key={elem.id}>
+            {elem.icon}
+            {elem.label}
+          </Item>
+        ))}
+      </AdvantagesList>
+    </Wrapper>
+  );
 }
 
-export default Advantages
+const Wrapper = styled("div")`
+  margin-top: 1rem;
+  padding-right:3rem;
+  display: flex;
+  height: 600px;
+  align-items: center;
+  background: url(${img});
+  background-size: cover;
+  justify-content:end;
+`;
+
+const Item = styled("li")`
+font-size:1.5rem;
+font-weight:300;
+  display: flex;
+  padding-bottom:1rem;
+  gap: 10px;
+`;
+const AdvantagesList = styled("ul")`
+color:white;
+gap:1rem;
+  border-radius: 10px;
+  background-color: #7a282c;
+  list-style: none;
+  padding: 0 2rem 2rem;
+  
+`;
+export default Advantages;
